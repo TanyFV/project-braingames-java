b55176940eb1d9a8ec06e234b2979da3dabe267c
Tatiana Fedorina <tanyfv@gmail.com>
2020-10-22 19:40:28 +0300

    add guard expression to PrimeNumber

--- src/main/java/games/PrimeNumbers.java
+++ src/main/java/games/PrimeNumbers.java
@@ -8,6 +8,8 @@ public class PrimeNumbers {
         String userName = Utils
                 .welcomeToBrainGames("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
         Utils.isCorrectNumber(userName, (val) -> {
+            if (val == 1 || val == 0)
+                return false;
             for (int i = 2; i < val / 2; i++) {
                 if (val % i == 0)
                     return false;
